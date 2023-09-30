ce2d8bf8811b633f14c465388be8c53c7dadc097
Estomihi100103 <estopangaribuan@gmail.com>
2023-09-30 21:13:49 +0700

    Update

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,38 @@
 // NIM - Your Name
+main()
+{
+    // Deklarasi variabel
+    int a, b, c;
+    // Input
+    System.out.print("Masukkan nilai a: ");
+    a = baca.nextInt();
+    System.out.print("Masukkan nilai b: ");
+    b = baca.nextInt();
+    System.out.print("Masukkan nilai c: ");
+    c = baca.nextInt();
+    // Proses
+    if (a > b)
+    {
+        if (a > c)
+        {
+            System.out.println("Nilai a terbesar");
+        }
+        else
+        {
+            System.out.println("Nilai c terbesar");
+        }
+    }
+    else
+    {
+        if (b > c)
+        {
+            System.out.println("Nilai b terbesar");
+        }
+        else
+        {
+            System.out.println("Nilai c terbesar");
+        }
+    }
+    // Output
+}
+```
